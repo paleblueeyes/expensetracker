794c05f292ab8e89cc52597fad66650f0c517be6
paleblueeyes <andreasrolfsvaag@gmail.com>
2020-05-13 17:19:38 +0200

    Updates

---
+++ precache-manifest.53357be8d37e348fb24293bdf9d46aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a7c881ab184e5df5c78c51e2e4b21d",
+    "url": "/expensetracker/index.html"
+  },
+  {
+    "revision": "305a7e0d87e31adab767",
+    "url": "/expensetracker/static/css/main.b136fc88.chunk.css"
+  },
+  {
+    "revision": "3e0e16b74ff9a904f6e2",
+    "url": "/expensetracker/static/js/2.37597736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expensetracker/static/js/2.37597736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305a7e0d87e31adab767",
+    "url": "/expensetracker/static/js/main.2c93f794.chunk.js"
+  },
+  {
+    "revision": "7b816f277fecea82e82f",
+    "url": "/expensetracker/static/js/runtime-main.918b67f6.js"
+  }
+]);
